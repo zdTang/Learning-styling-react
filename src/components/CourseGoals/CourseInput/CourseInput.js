9317ca0c45ca7d150e3f87b2cb7cb0de65d10dd1
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2021-10-30 09:57:09 -0400

    Reset the color
    Use different in-line style to show feedback to user once it input a invalid
    
    also need reset the RED color to normal

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,33 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
-  const [isValid,setIsValid]=useState(true)             //  set boolean to trigger style
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true); //  set boolean to trigger style
 
-  const goalInputChangeHandler = event => {
+  const goalInputChangeHandler = (event) => {
+    if(event.target.value.trim().length>0)
+    {
+      setIsValid(true)        //  reset the color,   or once it turns RED, it will not change again
+    }
     setEnteredValue(event.target.value);
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
-    if(enteredValue.trim().length===0){
+    if (enteredValue.trim().length === 0) {
       //alert ("Goal can not be empty !")
-      setIsValid(false)
-    }
-    else{
+      setIsValid(false);
+    } else {
       props.onAddGoal(enteredValue);
     }
-    
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label style={{color: isValid?'green':'red'}}>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{ color: isValid ? "green" : "red" }}>Course Goal</label>
+        <input
+          type="text"
+          style={{
+            borderColor: isValid ? "black" : "red",
+            background: isValid ? "transparent" : "salmon",
+          }}
+          onChange={goalInputChangeHandler}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
